db7c015aa14a4cf0d9bf32a24af9dc3df4936f35
mariia <mariia@beamery.com>
2022-11-09 15:14:21 +0000

    sdet interview task

---
+++ tests/api.test.js
@@ -0,0 +1,12 @@
+import request from "supertest";
+
+const baseUrl = "https://sample_url";
+
+describe("Posts endpoint", () => {
+  it("should return a 200 status code when fetching 'posts/1' ", async () => {
+    // Write your code bellow
+
+    expect(false).toBe(true);
+  });
+
+});
